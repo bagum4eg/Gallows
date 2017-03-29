1d9a0bb6f278c812e58fef0acf87688042df9c02
bagum4eg <iistr1ke@mail.ru>
2017-03-29 18:04:23 +0400

    vrode vse norm

--- gallows.py
+++ gallows.py
@@ -29,6 +29,12 @@ def drow_word (secret_word, user_letters ):
 			word_to_drow.append("_")
 	return ''.join(word_to_drow)
 
+def alphabet_table (alphabet_, user_letter):
+	for letter in user_letter :
+		if letter in user_letter :			
+			alphabet_[alphabet_.index(user_letter)]=" "
+	return alphabet_
+       
 def main():
 	slovo = urllib.request.urlopen("http://www.setgetgo.com/randomword/get.php?q=4").read().decode("utf-8").lower()
 	print(slovo)
@@ -37,20 +43,34 @@ def main():
 	i=0
 	let=''
 	space=(' ')
-	print (drow_word(slovo, let))
+	print (drow_word(slovo, let),'\n ')
+	alphabet=[]
+	for j in range(97,124):
+		alphabet.append(chr(j))
+	for k in range(0,26):
+		print(alphabet[k], end=' ')
+		if (k+1) % 9 == 0:
+			print()
+	print()
 	while t>0:
-		print('\nу вас ',t,' попыток')    
-		let1=input('введите букву: ')
+		print('\nУ вас ',t,' попыток')    
+		let1=input('выбирайте букву: ')
 		if slovo.find(let1)!=-1 and len(let1)==1 and let1.isdigit()==False and let.find(let1)==-1:
 			cls()
-			print(drow_word(slovo, let + let1))
+			alphabet_table(alphabet, let1)
+			for k in range(0,26):
+				print(alphabet[k], end=' ')
+				if (k+1) % 9 == 0:
+				      print()
+			print()
+			print('\n ',drow_word(slovo, let + let1))			
 		elif let1.isdigit()==True or len(let1)!=1:
 			print("Мы играем в слова! Попробуйте ввести одну букву снова.")
 		elif let.find(let1)>-1:
 			print('Вы уже выбирали эту букву. Попробуйте другую.')
 		else:
 			t-=1
-			print('НЕ УГАДАЛ!! Попытайся еще!')
+			print('ВЫ НЕ УГАДАЛИ!! Попытайся еще!')
 		let=let + let1
 		if set(let).intersection(set(slovo)) == set(slovo):
 			print("Поздравляем! Вы выиграли, и на этот раз виселится останется без работы =(")
@@ -61,7 +81,7 @@ def main():
 	ch=input("Хотели бы вы сыграть еще раз?(Y/N)")
 	while m!=0:
 		if ch.lower()=="y":
-			m-=1
+			main()
 		elif ch.lower()=="n":
 			break
 		else:
